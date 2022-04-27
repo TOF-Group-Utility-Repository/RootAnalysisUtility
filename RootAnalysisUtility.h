8c4e59043bad371e47cc1400907fe893496a959c
Nicola Rubini <nicola.rubini@cern.ch>
2022-04-27 12:13:47 +0200

    Create general include file w/ commented structure

---
+++ RootAnalysisUtility.h
@@ -0,0 +1,25 @@
+//
+//  Part of the RootAnalysisUtility package
+//
+//  General File to be included in the individual analysis
+//
+//  Author              Nicola Rubini
+//  Created             27/04/2022
+//  Last modified       27/04/2022
+//
+#ifndef ROOTANALYSISUTILITY_H
+#define ROOTANALYSISUTILITY_H
+//
+//  --- --- --- ---
+//  --- Include package modules
+//  --- --- --- ---
+//
+//  --- --- --- ---
+//  --- Global Variables
+//  --- --- --- ---
+//
+//  --- --- --- ---
+//  --- Global Functions
+//  --- --- --- ---
+//
+#endif
